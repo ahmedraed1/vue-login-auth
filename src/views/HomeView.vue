cf66a11440955a7af4b7e33e0b923c009219394e
Ahmed Raed <ahmedraed101@yahoo.com>
2024-04-20 23:08:01 +0300

    designing-components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="w-full h-full flex justify-center items-center">
+    <component
+      :is="components[component]"
+      @signUp="component = 'signUp'"
+      @signIn="component = 'signIn'"
+    ></component>
   </main>
 </template>
+<script setup>
+import { ref } from 'vue'
+import signIn from '@/components/signIn.vue'
+import signUp from '@/components/signUp.vue'
+
+let components = {
+  signIn,
+  signUp
+}
+const component = ref('signIn')
+
+// const go = function (methodType) {
+//   component.value = methodType
+// }
+</script>
